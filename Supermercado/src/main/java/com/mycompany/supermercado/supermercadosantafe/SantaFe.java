1084bd635a18ed27e729e9c3dca691c44a7b3aed
David Ardila <davidardila6@hotmail.com>
2020-08-30 14:42:54 -0500

    Supermercado

---
+++ Supermercado/src/main/java/com/mycompany/supermercado/supermercadosantafe/SantaFe.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.supermercado.supermercadosantafe;
+
+/**
+ *
+ * @author david
+ */
+public class SantaFe {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
